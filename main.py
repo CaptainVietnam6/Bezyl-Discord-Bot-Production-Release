beb45ff4deb0cc97e8ff88fdaf225faa020f652e
CaptainVietnam6 <kiet.riley2005@gmail.com>
2021-05-06 03:36:55 -1000

    added license and copyright message

---
+++ main.py
@@ -0,0 +1,14 @@
+'''
+Copyright (©) 2021 Kiet Pham <kiet.riley2005@gmail.com>
+This software/program has a copyright license, more information is in the 'LICENSE' file
+IF YOU WANT TO USE/COPY/MODIFY/REPRODUCE/RE-DISTRIBUTE THIS PROGRAM, YOU MUST INCLUDE A COPY OF THE LICENSE
+
+Author Name: Kiet Pham, Junle Yan
+Author Contact: kiet.riley2005@gmail.com, yanjunlereal@gmail.com
+Discord: CaptainVietnam6#0001, wholefood_doufu#9523
+Discord Server: https://discord.gg/3z76p8H5yj
+GitHub: https://github.com/CaptainVietnam6
+GitHub Repo: https://github.com/CaptainVietnam6/Bezyl-Discord-Bot-Production-Release
+Instagram: @itz_kietttttttttt
+Program Status: ACTIVE - Production Release
+'''
